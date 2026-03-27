ac515c73eee504c5091eea4fb0466c2fac53db20
basslerfish <mathis@eduvpn-uva-mdw-145-3-8-198.vpn.uva.nl>
2026-03-27 17:24:56 +0100

    fix cluster bug

--- scripts/2d/cluster/search_hyperparams.py
+++ scripts/2d/cluster/search_hyperparams.py
@@ -17,6 +17,12 @@ from cancer_ml.utils import assert_gpu_available
 MAX_TRIALS = 50
 BATCH_SIZE = 64
 N_EPOCHS = 50
+FILTER_SIZES = {
+    "16-32": [16, 32],
+    "16-32-64": [16, 32, 64],
+    "32-64": [32, 64],
+    "32-64-128": [32, 64, 128]
+}
 
 
 def main() -> None:
@@ -65,10 +71,12 @@ def main() -> None:
 
     # func
     def build_model(hp: kt.HyperParameters) -> keras.Model:
+        fs_name = hp.Choice("filter_sizes", list(FILTER_SIZES.keys()))
+        filter_sizes = FILTER_SIZES[fs_name]
         model = get_flexible_model(
             input_shape=input_shape,
             model_type=hp.Choice("model_type", ["simple", "advanced"]),
-            filter_sizes=hp.Choice("model_type", possible_fs),
+            filter_sizes=filter_sizes,
             add_skips=hp.Choice("add_skips", [False, True])
         )
         optimizer = keras.optimizers.Adam()
--- scripts/2d/search_hyperparams.py
+++ scripts/2d/search_hyperparams.py
@@ -13,9 +13,15 @@ from cancer_ml.models.loss import DiceBCELoss
 # params
 MAX_TRIALS = 50
 OUTPUT = Path("/Users/mathis/Code/private_projects/cancer_ml/results/params_search")
-DSET_FOLDER = Path("/Users/mathis/Code/private_projects/cancer_ml/results/datasets/flattened")
+DSET_FOLDER = Path("/Users/mathis/Code/private_projects/cancer_ml/results/datasets/2d/samples500_val15_test15_128-128")
 BATCH_SIZE = 64
 N_EPOCHS = 50
+FILTER_SIZES = {
+    "16-32": [16, 32],
+    "16-32-64": [16, 32, 64],
+    "32-64": [32, 64],
+    "32-64-128": [32, 64, 128]
+}
 
 # load data
 print("---Load data---")
@@ -37,18 +43,14 @@ input_shape = X.shape
 
 
 print("---Run optimization---")
-# define possible filter sizes
-possible_fs = []
-possible_fs.append([16, 32])
-possible_fs.append([32, 64])
-possible_fs.append([32, 64, 128])
-
 # func
 def build_model(hp: kt.HyperParameters) -> keras.Model:
+    fs_name = hp.Choice("filter_sizes", list(FILTER_SIZES.keys()))
+    filter_sizes = FILTER_SIZES[fs_name]
     model = get_flexible_model(
         input_shape=input_shape,
         model_type=hp.Choice("model_type", ["simple", "advanced"]),
-        filter_sizes=hp.Choice("model_type", possible_fs),
+        filter_sizes=filter_sizes,
         add_skips=hp.Choice("add_skips", [False, True])
     )
     optimizer = keras.optimizers.Adam()
