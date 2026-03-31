f9be5c0d68cc6098c596c3768bd6cab004c629c8
basslerfish <mathis@eduvpn-uva-mdw-145-3-8-123.vpn.uva.nl>
2026-03-31 15:00:32 +0200

    try fix unfreeze

--- cancer_ml/models/callbacks.py
+++ cancer_ml/models/callbacks.py
@@ -11,18 +11,12 @@ class UnfreezeCallBack(keras.callbacks.Callback):
     def __init__(
             self,
             model: keras.Model,
-            loss_fn: keras.losses.Loss,
-            optimizer: keras.optimizers.Optimizer,
-            metrics: list,
             epoch_to_unfreeze: int = 20,
     ) -> None:
         super().__init__()
         self.epoch_to_unfreeze = epoch_to_unfreeze
         self.fit_elements = {
             "model": model,
-            "loss": loss_fn,
-            "optimizer": optimizer,
-            "metrics": metrics,
         }
 
     def on_epoch_begin(self, epoch, logs=None) -> None:
@@ -31,7 +25,7 @@ class UnfreezeCallBack(keras.callbacks.Callback):
             model = self.fit_elements["model"]
             model.trainable = True
             model.compile(
-                optimizer=self.fit_elements["optimizer"],
-                metrics=self.fit_elements["metrics"],
-                loss=self.fit_elements["loss"]
+                optimizer=model.optimizer,
+                metrics=model.metrics,
+                loss=model.loss,
             )
